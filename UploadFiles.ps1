75e823b3f46394d1a33f6ebe11debf4a6ac75e48
wrass <wrass@users.noreply.github.com>
2020-03-03 10:42:08 +0000

    Update UploadFiles.ps1

--- UploadFiles.ps1
+++ UploadFiles.ps1
@@ -1,163 +1,164 @@
-# Import files to SharePoint Online Site document library
-#  João Mendes - 7/9/2018
+# import files to SharePoint online document library
+# Ti Marner - 2020-02-27
 # Parameters:
-#  Site Url
-#  csv File:
-#-----------------------------------------
-#  
+#   siteurl
+#   csvfile
+#---------------------------------------------
+#
 [CmdletBinding()]
     Param(
         [Parameter(Mandatory = $true)]
         [string]$siteUrl,
         [Parameter(Mandatory = $true)]
-        [string]$fileName
+        [string]$csvFile
     )
-#********************
-# Run Function
-#********************    
+# Function
 function ImportFile {
-    
-
-    # Teste if module SharePointPnPPowerShellOnline exists
-    if ((Get-Module -ListAvailable -Name SharePointPnPPowerShellOnline) -eq $null) {
-        Install-Module SharePointPnPPowerShellOnline -force
+    function Logmsg {
+        Param($msg)
+        Write-Host $msg
+        Add-Content -Value $msg -Path $logfilename
     }
-    clear-host
-    # Teste if file exists
-    $results = Test-Path $fileName
+
+    Clear-Host
+
+    $results = Test-Path $csvFile
     if ($results -eq $false) {
-        throw " O Ficheiro $fileName com informação a importar não existe."
-        exit 1
-    }
-    # teste se PC is em pt-PT Culture
-    $culture = Get-Culture
-    if ($culture.Name -ne "pt-PT"){
-        throw " O computador não está com Regional Settings defindo para Portugal, (pt-PT) necessário para a execução da Importação."
+        throw "File $csvFile does not exist"
         exit 1
     }
 
-    # Define LogFile
-    $date = get-date -Format "yyyyMMddhhmmss"
-    $logfilename = "FilesNotUpload_ $fileName_$date.log"
-    write-output " " 
-    add-content  -value " " -path $logfilename
-    Add-Content -value "A Importação de Ficheiros começou às  $(Get-date)" -path $logfilename -Encoding Oem
-    write-output "A Importação de Ficheiros começou às  $(Get-date)"
-    Add-Content  -value "CSV file: $fileName"  -path $logfilename -Encoding UTF8
-    write-output "CSV file: $fileName"
-    Add-content -value "SharePoint Site: $siteUrl"  -path $logfilename -Encoding UTF8
-    Write-Output "SharePoint Site: $siteUrl"
-    Add-content -value " "  -path $logfilename -Encoding UTF8
-   
-    # Get Columns and CSV data 
-    $fileDataFile = import-csv -path $fileName -Delimiter ";"
-    #Get columns Names from CSV
-    $fileCoumns = (Get-Content -Path $fileName | Select-Object -First 1) -split ";"
-   # Connect SharePoint Online
-    Connect-PnPOnline $siteUrl -ErrorAction Stop
+    $culture = Get-Culture
+    if ($culture.Name -ne "en-GB") {
+        throw "Culture is not en-GB, stopping"
+        exit 1
+    }
+
+    # define logfile
+    $logfilename = "importFilesLog.log"
+    try {
+        $logFile = New-Item -Path . -Name $logfilename -ItemType "file" -Force
+    } catch {
+        throw "Can't write to $logfilename"
+        exit 1
+    }
+    Logmsg -msg "File import started at $(Get-Date)"
+    Logmsg -msg "CSV file: $csvFile"
+    Logmsg -msg "SharePoint site: $siteUrl"
+
+    # get columns and CSV data
+    $csvData = Import-Csv -Path $csvFile -Delimiter ";"
+    # get column names
+    $csvColumnNames = (Get-Content -Path $csvFile | Select-Object -First 1) -split ";"
     
-    # readfiles from CSV 
+    # connect to SharePoint online
+    Logmsg -msg "Connecting to SharePoint Online"
+    try {
+        Connect-PnPOnline -Url $siteUrl
+    } catch {
+        Write-Host "Unable to connect to $siteUrl" -ForegroundColor Black -BackgroundColor Red
+        exit 1
+    }
+
+    # read files from CSV
     $i = 0
-    foreach ($fileData in $fileDataFile) {
-        Write-Output "---"
-        write-output " Upload file: $i"
-        Write-output $fileData
-        # Testa se Ficheiro a importar existe
-    
-        $results = Test-Path $fileData.LocalizacaoAtual
+    foreach($csvRow in $csvData) {
+        Logmsg -msg "Uploading file $($i): $($csvRow.sourcePath)"
+        #Logmsg -msg $csvRow
+
+        # test if source file exists
+        $results = Test-Path $csvRow.sourcePath
         if ($results -eq $false) {
-            $msg = " $(get-date -format "yyyy/MM/dd") - Error - Ficheiro a Importar não encontrado, Path: " + $fileData.LocalizacaoAtual
-            Add-content -value  $msg -path $logfilename
-            Write-output $msg
-        }
-        else {
-            # Doclib Exist ?
-            $doclibExists = get-pnpList $fileData.BibliotecaDestino 
-            
-            if ($doclibExists -eq $null ) {
-                $msg = " $(get-date -format "yyyy/MM/dd") - Error - A Biblioteca  " + $fileData.BibliotecaDestino + " não existe no Site:" + $siteUrl
-                Add-content -value  $msg -path $logfilename
-                Write-output $msg
-            }
-            else {
-                $fileData.BibliotecaDestino.trim()
-                $_libcol = get-pnpfield -List $fileData.BibliotecaDestino.trim()
-                # Array objects Hastable com valores de Metadados 
+            Logmsg -msg "$(Get-Date -Format "yyyy-MM-dd") - Error - file not found, Path: $($csvRow.sourcePath)"
+        } else {
+            # source exists, test if document library exists
+            $docLibExists = Get-PnPList $csvRow.destLibrary
+
+            if ($null -eq $docLibExists) {
+                Logmsg -msg "$(Get-Date -Format "yyyy-MM-dd") - Error - library $($csvRow.destLibrary) does not exist in site: $siteUrl"
+            } else {
+                # doc library is good, get columns
+                $libraryColumns = Get-PnPField -List $csvRow.destLibrary.trim()
+                
+                # metadata hash table
                 $values = @{}
-                for ($j = 3; $j -lt $fileCoumns.Count; $j++) {
+                for ($j = 5; $j -lt $csvColumnNames.length; $j++) {
                     $addColumnToValues = $true
-                    $_column = $fileCoumns[$j]
-                    $_columnValue = $fileData.$_column.trim()
-                    $columnDef = $_libcol | Where-Object { $_.InternalName -eq $_column}                
-                    if ($columnDef -ne $null) {
-                        if ($columnDef.TypeAsString -eq "Lookup") {
-                            if ($_columnValue -eq $null) {
+                    $csvColumnName = $csvColumnNames[$j]
+                    $columnValue = $csvRow.$csvColumnName.trim()
+                    $libraryColumn = $libraryColumns | Where-Object {$_.Title -eq $csvColumnName}
+
+                    if ($null -ne $libraryColumn) {
+                        # deal with lookup types
+                        if ($libraryColumn.TypeAsString -eq "Lookup") {
+                            if ($null -eq $columnValue) {
                                 $addColumnToValues = $false
-                            }
-                            else {
-                                $listId = $columnDef.LookupList
-                                $listItem = (Get-PnPListItem -List $listId).FieldValues | Where-Object {$_.Title -eq $_columnValue.trim()}
-                                if (  $null -eq $listItem) {
-                                    $msg = " $(get-date -format "yyyy/MM/dd") - Error - Loja: " + $_columnValue + " Não existe na lista de Lojas "  
-                                    Add-content -value $msg -path $logfilename
-                                    Write-Output $msg
+                            } else {
+                                $listId = $libraryColumn.LookupList
+                                $listItem = (Get-PnPListItem -List $listId).FieldValues | Where-Object {$_.Title -eq $columnValue.trim()}
+                                if ($null -eq $listItem) {
+                                    Logmsg -msg "$(Get-Date -Format "yyyy-MM-dd") - Error - store: $columnValue does not exist in the list of stores"
                                     $addColumnToValues = $false
-                                }
-                                else {               
-                                    $_columnValue = $listItem.ID
+                                } else {
+                                    $columnValue = $listItem.ID
                                     $addColumnToValues = $true
                                 }
                             }
                         }
-                        # Format Date columns to correct Culture
-                        if ($columnDef.TypeAsString -eq "DateTime") {  
-                            if ( $_columnValue.trim() -ne $null) {
-                                #try convert )
-                                $_columnValue = get-date($_columnValue) -format "MM/dd/yyyy" -ErrorAction Continue
+
+                        # format date columns to correct culture
+                        if ($libraryColumn.TypeAsString -eq "DateTime") {
+                            if (($null -ne $columnValue.trim()) -and ($columnValue.trim() -ne "")) {
+                                # try to convert
+                                $dt = Get-Date($columnValue) -format $culture.DateTimeFormat.ShortDatePattern
+                                $columnValue = [datetime]::ParseExact($dt, $culture.DateTimeFormat.ShortDatePattern, $null)
+                                #Logmsg -msg "DateTime value is $columnValue"
                                 $addColumnToValues = $true
-                            }else{
+                            } else {
                                 $addColumnToValues = $false
                             }
                         }
-                    }
-                    # se coluna valida add 
-                    if ( $addColumnToValues -eq $true) {
-                        $values.add($_column.trim(), $_columnValue)
-                    }
-                }
-                # Upload do ficheiro
-                try {
-                    $_doclibName = $fileData.BibliotecaDestino 
-                    $_contenttype = '"' + $fileData.ContentType + '"'
-                    $listCtypes = Get-PnPContentType -List  $_doclibName 
-                    foreach ($ct in $listCtypes) {
-                        if ($ct.Name -eq $fileData.ContentType) {
-                            $_ctype = $ct.Id
+
+                        #Logmsg -msg "column $csvColumnName, TypeAsString is $($libraryColumn.TypeAsString), InternalName is $($libraryColumn.InternalName)"
+
+                        # add column to collection if it's valid
+                        # use the internal column name in this library
+                        if ($addColumnToValues -eq $true) {
+                            $values.Add($libraryColumn.InternalName, $columnValue)
                         }
-                    }  
-                    $ctr = Get-PnPContentType -List $fileData.BibliotecaDestino -Identity $_ctype.StringValue
-                    Set-PnPDefaultContentTypeToList -List $fileData.BibliotecaDestino -ContentType $ctr.Name 
-                    $added = add-pnpfile -Path $fileData.LocalizacaoAtual -Folder  $_doclibName -Values $values  -Checkout  
+                    }
                 }
-                catch {
-                    $msg = " $(get-date -format "yyyy/MM/dd") - Erro ao fazer uplaod do ficheiro para o SharePoint Site:" + $siteUrl + " Document Library: " + $fileData.BibliotecaDestino + " , Ficheiro: " + $fileData.LocalizacaoAtual + " Erro:" + $($_.Exception.Message)
-                    Add-content -value $msg -path $logfilename
-                    Write-Output $msg
+
+                # do file upload
+                try {
+                    # ensure destination folder exists
+                    $destPath = $csvRow.destLibrary + "/" + $csvRow.destFolder
+                    $siteFolder = Resolve-PnPFolder -SiteRelativePath $destPath
+
+                    # add file
+                    if ($null -eq $siteFolder) {
+                        Logmsg -msg "$(Get-Date -Format "s") - Error uploading file to SharePoint site: $siteUrl, Document Library: $($csvRow.destLibrary), file: $($csvRow.sourcePath), Unable to create folder: $destPath"
+                    } else {
+                        # set content type for file
+                        #$values.Add("ContentTypeId", $???)
+                        #Write-Host "-Path "+$csvRow.sourcePath+" -Folder "+$destPath+" -NewFileName "+$csvRow.fileName+" -Values "+$values
+                        $null = Add-PnPFile -Path $csvRow.sourcePath -Folder $destPath -NewFileName $csvRow.fileName -Values $values #-ContentType $csvRow.contentType
+                    }
+                } catch {
+                    Logmsg -msg "$(Get-Date -Format "s") - Error uploading file to SharePoint site: $siteUrl, Document Library: $($csvRow.destLibrary), file: $($csvRow.sourcePath), Error: $($_.Exception.Message)"
                 }
-                # }
             }
         }
 
-        $i += 1  
+        $i += 1
     }
-    add-content "  " -path $logfilename
-    $msg = "--- Fim da Importação as $(get-date) "
-    Add-content -value $msg -path $logfilename
-    Write-Output $msg  
-    # Disconnect-PnPOnline
+
+    Logmsg -msg "--- Import finished at $(Get-Date)"
+
+    Disconnect-PnPOnline
 }
+
 #
-### Import Files to SharePoint Site ####
+### Import files to SharePoint site ###
 # Main #
 ImportFile
- 
